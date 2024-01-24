81bd3e35fe5b19eba13a720848f7ec0826345a3e
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-01-24 14:24:31 -0500

    fixed navbar dropdown bg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,8 +59,8 @@ const Navbar = () => {
             {/* Full Navbar for All Screens */}
             <div className={`mt-4 xl:mt-0 xl:pt-5 ${isMenuOpen ? 'flex' : 'hidden'} xl:flex flex-col space-y-4 xl:space-y-0 w-full`}>
                 {/* Cat Food & Nutrition Dropdown */}
-                <div className="dropdown" >
-                    <div className={`hover:text-white py-2 flex justify-between mx-2 ${openDropdown === 'food-nutrition' ? 'text-white' : ''}`}  onClick={() => toggleDropdown('food-nutrition')} >                    
+                <div className="dropdown " >
+                    <div className={`hover:text-white py-2 flex justify-between mx-2 xl:pl-2 ${openDropdown === 'food-nutrition' ? 'text-white' : ''}`}  onClick={() => toggleDropdown('food-nutrition')} >                    
                         <div>
                             <Link to="/food-nutrition">Food & Nutrition</Link>
                         </div>
@@ -68,7 +68,7 @@ const Navbar = () => {
                     </div>
                     {/* Dropdown Menu */}
                     {openDropdown === 'food-nutrition' && (
-                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3]">
+                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3] xl:pl-2">
                             <Link to="/food-nutrition#requirements" className="block px-6 py-2 hover:bg-mid-green">Nutritional Requirements</Link>
                             <Link to="/food-nutrition#recommended-foods" className="block px-6 py-2 hover:bg-mid-green">Recommended Foods</Link>
                             <Link to="/food-nutrition#special-diets" className="block px-6 py-2 hover:bg-mid-green">Special Diets</Link>
@@ -80,14 +80,14 @@ const Navbar = () => {
 
                 {/* Cat Litter & Hygiene Dropdown */}
                 <div className="dropdown">
-                    <div className={`hover:text-white py-2 flex justify-between mx-2 ${openDropdown === 'litter-hygiene' ? '' : ''}`} onClick={() => toggleDropdown('litter-hygiene')} >                    
+                    <div className={`hover:text-white py-2 flex justify-between mx-2 xl:pl-2 ${openDropdown === 'litter-hygiene' ? '' : ''}`} onClick={() => toggleDropdown('litter-hygiene')} >                    
                         <div>
                             <Link to="/litter-hygiene">Litter & Hygiene</Link>
                         </div>
                         <IoIosArrowDown className={`block min-h-6 min-w-6 transform transition duration-300 ease-in-out ${openDropdown === 'litter-hygiene' ? '-rotate-180' : ''}`}/>                    
                     </div>
                     {openDropdown === 'litter-hygiene' && (
-                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3]">
+                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3] xl:pl-2 ">
                         <Link to="/litter-hygiene#types-of-litter" className="py-2 px-6 block hover:bg-mid-green">Types of Litter</Link>
                         <Link to="/litter-hygiene#litter-box-maintenance" className="py-2 px-6 block hover:bg-mid-green">Litter Box Maintenance</Link>
                         <Link to="/litter-hygiene#litter-box-training" className="py-2 px-6 block hover:bg-mid-green">Litter Box Training</Link>
@@ -99,14 +99,14 @@ const Navbar = () => {
 
                 {/* Behavior & Training Dropdown */}
                 <div className="dropdown">
-                    <div className={`hover:text-white py-2 flex justify-between mx-2 ${openDropdown === 'behavior-training' ? '' : ''}`} onClick={() => toggleDropdown('behavior-training')} >                    
+                    <div className={`hover:text-white py-2 flex justify-between mx-2 xl:pl-2 ${openDropdown === 'behavior-training' ? '' : ''}`} onClick={() => toggleDropdown('behavior-training')} >                    
                         <div>
                             <Link to="/behavior-training">Behavior & Training</Link>
                         </div>
                         <IoIosArrowDown className={`block min-h-6 min-w-6  transform transition duration-300 ease-in-out ${openDropdown === 'behavior-training' ? '-rotate-180' : ''}`}/>                    
                     </div>
                     {openDropdown === 'behavior-training' && (
-                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3]">
+                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3] xl:pl-2">
                         <Link to="/behavior-training#understanding-behavior" className="py-2 px-6 block hover:bg-mid-green">Understanding Behavior</Link>
                         <Link to="/behavior-training#training-basics" className="py-2 px-6 block hover:bg-mid-green">Training Basics</Link>
                         <Link to="/behavior-training#behavioral-issues" className="py-2 px-6 block hover:bg-mid-green">Behavioral Issues</Link>
@@ -118,14 +118,14 @@ const Navbar = () => {
 
                 {/* Health & Wellness Dropdown */}
                 <div className="dropdown">
-                    <div className={`hover:text-white py-2 flex justify-between mx-2 ${openDropdown === 'health-wellness' ? '' : ''}`} onClick={() => toggleDropdown('health-wellness')} >                    
+                    <div className={`hover:text-white py-2 flex justify-between mx-2 xl:pl-2 ${openDropdown === 'health-wellness' ? '' : ''}`} onClick={() => toggleDropdown('health-wellness')} >                    
                         <div>
                             <Link to="/health-wellness">Health & Wellness</Link>
                         </div>
                         <IoIosArrowDown className={`block min-h-6 min-w-6  transform transition duration-300 ease-in-out ${openDropdown === 'health-wellness' ? '-rotate-180' : ''}`}/>                    
                     </div>
                     {openDropdown === 'health-wellness' && (
-                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3]">
+                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3] xl:pl-2">
                         <Link to="/health-wellness#common-health-issues" className="py-2 px-6 block hover:bg-mid-green">Common Health Issues</Link>
                         <Link to="/health-wellness#preventative-care" className="py-2 px-6 block hover:bg-mid-green">Preventative Care</Link>
                         <Link to="/health-wellness#vaccinations" className="py-2 px-6 block hover:bg-mid-green">Vaccinations</Link>
@@ -137,14 +137,14 @@ const Navbar = () => {
                 </div>
                 {/* Cat Breeds Dropdown */}
                 <div className="dropdown">
-                    <div className={`hover:text-white py-2 flex justify-between mx-2 ${openDropdown === 'breeds' ? '' : ''}`} onClick={() => toggleDropdown('breeds')} >                    
+                    <div className={`hover:text-white py-2 flex justify-between mx-2 xl:pl-2 ${openDropdown === 'breeds' ? '' : ''}`} onClick={() => toggleDropdown('breeds')} >                    
                         <div>
                             <Link to="/breeds">Breeds</Link>
                         </div>
                         <IoIosArrowDown className={`block min-h-6 min-w-6  transform transition duration-300 ease-in-out ${openDropdown === 'breeds' ? '-rotate-180' : ''}`}/>                    
                     </div>
                     {openDropdown === 'breeds' && (
-                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3]">
+                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3] xl:pl-2">
                         <Link to="/breeds#overview" className="py-2 px-6 block hover:bg-mid-green">Overview of Breeds</Link>
                         <Link to="/breeds#breed-specific-care" className="py-2 px-6 block hover:bg-mid-green">Breed-Specific Care</Link>
                         <Link to="/breeds#choosing-right-breed" className="py-2 px-6 block hover:bg-mid-green">Choosing the Right Breed</Link>                        
@@ -153,14 +153,14 @@ const Navbar = () => {
                 </div>
                 {/* Product Reviews & Recommendations Dropdown */}
                 <div className="dropdown">
-                    <div className={`hover:text-white py-2 flex justify-between mx-2 ${openDropdown === 'product-reviews' ? '' : ''}`} onClick={() => toggleDropdown('product-reviews')} >                    
+                    <div className={`hover:text-white py-2 flex justify-between mx-2 xl:pl-2 ${openDropdown === 'product-reviews' ? '' : ''}`} onClick={() => toggleDropdown('product-reviews')} >                    
                         <div>
                             <Link to="/product-reviews">Product Reviews</Link>
                         </div>
                         <IoIosArrowDown className={`block min-h-6 min-w-6  transform transition duration-300 ease-in-out ${openDropdown === 'product-reviews' ? '-rotate-180' : ''}`}/>                    
                     </div>
                     {openDropdown === 'product-reviews' && (
-                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3]">
+                        <div className="dropdown-menu mt-2 py-2 bg-[#a3bba3] xl:pl-2">
                         <Link to="/product-reviews#food-treats" className="py-2 px-6 block hover:bg-mid-green">Food and Treats</Link>
                         <Link to="/product-reviews#litter-boxes" className="py-2 px-6 block hover:bg-mid-green">Litter and Litter Boxes</Link>
                         <Link to="/product-reviews#toys" className="py-2 px-6 block hover:bg-mid-green">Toys and Enrichment</Link>
@@ -172,7 +172,7 @@ const Navbar = () => {
 
                 {/* Additional Single Link - About */}
                 <div>
-                    <Link to="/about" className="hover:text-white py-2 flex justify-between mx-2">
+                    <Link to="/about" className="hover:text-white py-2 xl:pl-2 flex justify-between mx-2">
                         <div>
                             <h1>About</h1>
                         </div>
