253a53081616a2e2046696de69c568b4f44c6740
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-01-30 22:20:17 -0500

    enhance footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className=" text-black text-center mt-16 xl:ml-60 xl:mt-0 fixed bottom-0 left-0 right-0">
-            <div className="container mx-auto">
+        <footer className="text-black text-center mt-16 xl:ml-64 xl:mt-0 fixed bottom-0 left-0 right-0">
+            <div className="border-t border-mid-green w-10/12 mx-auto"></div>
+            <div className="container mx-auto pt-4">
                 <p>&copy; {new Date().getFullYear()} My Website</p>
                 <p>Follow us on social media:</p>
                 <div className="flex justify-center space-x-4 pb-4 pt-2">
