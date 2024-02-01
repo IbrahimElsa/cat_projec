715f446f1e9d87368080c907b2516b63f04f1f5f
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-01-31 21:29:12 -0500

    Update Nutrition page layout and content

--- src/pages/Nutrition.js
+++ src/pages/Nutrition.js
@@ -3,19 +3,26 @@ import Navbar from '../components/Navbar';
 
 function Nutrition() {
     return (
-        <div>
+        <div className="flex">
             <Navbar />
-            <div className="grid grid-cols-4 gap-4">
-                <div className="col-span-1 flex flex-col justify-center items-center">
+            <div className="xl:ml-64">
+                <div className="flex flex-col justify-center items-center">
                 </div>
-                <div className="col-span-3">
-                    <h1>Section 1</h1>
-                    <h1>Section 2</h1>
+                <div className="">
+                    <h1 className="text-2xl mb-1">Nutritional Requirements</h1>
+                    <p className="text-base mt-1">Section Nutritional</p>
+                    <h1 className="text-2xl mb-1">Recommended Foods</h1>
+                    <p className="text-base mt-1">Section Recommended</p>
+                    <h1 className="text-2xl mb-1">Special Diets</h1>
+                    <p className="text-base mt-1">Section Diets</p>
+                    <h1 className="text-2xl mb-1">Food Safety and Storage</h1>
+                    <p className="text-base mt-1">Section Safety and Storage</p>
+                    <h1 className="text-2xl mb-1">Food Reviews</h1>
+                    <p className="text-base mt-1">Section Reviews</p>
                 </div>
             </div>
         </div>
     );
 }
-
 
 export default Nutrition;
