93452bda4fe41d3c70bf67c4db9da1dbe7f1c403
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-02-03 22:48:10 -0500

    Add cat breed and healthy cat images to Main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Cat_breed_main from '../assets/Cat_breed_main.webp';
+import healthy_cat from '../assets/healthy-cat.jpeg';
 
 function Main() {
 return (
@@ -17,19 +19,27 @@ return (
             <section className="my-8 px-4">
                 <h3 className="text-2xl font-semibold">Learn About Cats</h3>
                     <p className="mt-2">
-                        Discover fascinating facts about cats, understand their care needs, and explore tips on nutrition, hygiene
-                        general health tips and more.
+                        Discover fascinating facts about cats, understand their care needs, and explore tips on nutrition, hygiene,
+                        general health, and more.
                     </p>
             </section>
 
             {/* Topic Links */}
             <section className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 my-8">
                 {/* Each of these divs represents a topic link */}
-                <div className="bg-gray-300 p-6 rounded-lg shadow-lg">
-                    <h4 className="font-semibold text-lg">Cat Breeds</h4>
-                    <p>Explore various cat breeds.</p>
+                <div className="bg-gray-300 p-6 rounded-lg shadow-lg"      style={{ 
+                        backgroundImage: `url(${Cat_breed_main})`, 
+                        backgroundSize: 'cover', 
+                        backgroundPosition: 'center' 
+                    }}>
+                    <h4 className="font-semibold text-lg text-soft-cream">Cat Breeds</h4>
+                    <p className='text-soft-cream'>Explore various cat breeds.</p>
                 </div>
-                <div className="bg-gray-300 p-6 rounded-lg shadow-lg">
+                <div className="bg-gray-300 p-6 rounded-lg shadow-lg"      style={{ 
+                        backgroundImage: `url(${healthy_cat})`, 
+                        backgroundSize: 'cover', 
+                        backgroundPosition: 'center' 
+                    }}>
                     <h4 className="font-semibold text-lg">Cat Care</h4>
                     <p>Learn how to take care of your cat.</p>
                 </div>
