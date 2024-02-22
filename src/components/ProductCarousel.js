81a58046b8d2fb5e8d3e73fc323050f5bbaa6229
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-02-21 22:50:58 -0500

    Add react-multi-carousel library for responsive product carousel

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 
 const ProductCarousel = ({ reviews }) => {
-    return (
-        <div className="max-w-md mx-auto"> {/* Adjust the max width as needed */}
-            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
-                {reviews.map((review, index) => (
-                    <div key={index}>
-                        <img src={review.imageSrc} alt={review.title} className="h-auto" />
-                        <p className="legend">{review.title}</p>
-                    </div>
-                ))}
-            </Carousel>
+  return (
+    <Carousel responsive={responsive}>
+      {reviews.map((review, index) => (
+        <div key={index}>
+          <img src={review.imageSrc} alt={review.title} className="h-auto" />
+          <p className="legend">{review.title}</p>
         </div>
-    );
+      ))}
+    </Carousel>
+  );
 };
 
 export default ProductCarousel;
